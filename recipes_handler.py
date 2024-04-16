ad60f211648d7c11707d535a8bebf91298b18fac
izorginpasha <izorgin.pasha@yandex.ru>
2024-04-16 17:45:44 +0300

    комментарий к ашÑfinal

--- recipes_handler.py
+++ recipes_handler.py
@@ -1,22 +1,22 @@
+import asyncio
 import aiohttp
 from googletrans import Translator
 from random import choices
 from aiogram.filters import Command, CommandObject
 from aiogram.types import Message
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from aiogram.utils.formatting import (
-    Bold, as_list, as_marked_section
-)
+
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram import Router, types
 
 router = Router()
+translator = Translator()
 
 
 class OrderWeather(StatesGroup):
     waiting_for_forecast = State()
-
+    waiting = State()
 
 
 async def categories(session):
@@ -24,9 +24,8 @@ async def categories(session):
             url=f'https://www.themealdb.com/api/json/v1/1/list.php?c=list',
     ) as resp:
         data = await resp.json()
-        print(data['meals'])
+
         data_dates = [item['strCategory'] for item in data['meals']]
-        print(data_dates)
 
     return data_dates
 
@@ -36,12 +35,19 @@ async def list_recipes(session, category, i):
             url=f'https://www.themealdb.com/api/json/v1/1/filter.php?c={category}',
     ) as resp:
         data = await resp.json()
-        print(data)
-        data = choices(data['meals'], k=i)
-        print(data)
 
+        data = choices(data['meals'], k=int(i))
 
     return data
+
+
+async def list_recipes_txt(session, id):
+    async with session.get(
+            url=f'https://www.themealdb.com/api/json/v1/1/lookup.php?i={id}',
+    ) as resp:
+        data = await resp.json()
+
+    return data['meals']
 
 
 @router.message(Command("category_search_random"))
@@ -71,7 +77,35 @@ async def weather_by_date(message: types.Message, state: FSMContext):
     data = await state.get_data()
     async with aiohttp.ClientSession() as session:
         list = await list_recipes(session, message.text, data['count'])
+    await state.set_data({'list': list})
+    builder = ReplyKeyboardBuilder()
+    builder.add(types.KeyboardButton(text="Покажи рецепты"))
+
+    builder.adjust(4)
+
     await message.answer(
-        f"Погода в городе {data['count']} в {message.text}:  "
+
+        f"Как вам такие варианты: {[translator.translate(item['strMeal'], dest='ru').text for item in list]} "
+        f"из {translator.translate(message.text, dest='ru').text}:  ",
+        reply_markup=builder.as_markup(resize_keyboard=True),
 
     )
+    await state.set_state(OrderWeather.waiting.state)
+
+
+@router.message(OrderWeather.waiting)
+async def mess(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+
+    async with aiohttp.ClientSession() as session:
+        for meal in data['list']:
+            instructions = await asyncio.gather(list_recipes_txt(session, int(meal['idMeal'])))
+
+            await message.answer(
+                f"Рецепт : {translator.translate(instructions[0][0]['strMeal'], dest='ru').text} "
+                f"                                                         "
+                f" {translator.translate(instructions[0][0]['strInstructions'], dest='ru').text}"
+                f"                                                         "
+                f" Ингредиенты : {[translator.translate(instructions[0][0][f'strIngredient{i + 1}'], dest='ru').text for i in range(20) if instructions[0][0][f'strIngredient{i + 1}'] != '']} ",
+
+            )
