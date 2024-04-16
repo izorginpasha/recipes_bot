39ee00429c5a4b492f49d96073dd4bb6a0e63363
izorginpasha <izorgin.pasha@yandex.ru>
2024-04-16 10:00:48 +0300

    комментарий к коммиту

---
+++ bot_main.py
@@ -0,0 +1,64 @@
+import asyncio
+
+import logging
+import sys
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram import F
+from recipes_handler import router
+from aiogram.utils.formatting import (
+    Bold, as_list, as_marked_section
+)
+
+from token_data import TOKEN
+
+dp = Dispatcher()
+dp.include_router(router)
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    kb = [
+        [
+            types.KeyboardButton(text="Команды"),
+            types.KeyboardButton(text="Описание бота"),
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+    )
+    await message.answer(f"Привет! С чего начнем?", reply_markup=keyboard)
+
+
+@dp.message(F.text.lower() == "команды")
+async def commands(message: types.Message):
+    response = as_list(
+        as_marked_section(
+            Bold("Команды:"),
+            "/category_search_random и число рецептов",
+
+            marker="✅ ",
+        ),
+    )
+    await message.answer(
+        **response.as_kwargs()
+    )
+
+
+@dp.message(F.text.lower() == "описание бота")
+async def description(message: types.Message):
+    await message.answer("Этот бот предоставляет информацию о рецептах блюд.")
+
+
+async def main() -> None:
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
